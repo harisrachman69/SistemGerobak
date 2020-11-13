ed6018e8102db321c8be2e3ad15a1940fbae5326
HarisRachman <60723912+harisrachman69@users.noreply.github.com>
2020-11-13 15:41:25 +0700

    Add files via upload
    gerobak_online.sql

---
+++ gerobak_online.sql
@@ -0,0 +1,209 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2020 at 09:40 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.2.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gerobak_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_barang`
+--
+
+CREATE TABLE `tbl_barang` (
+  `id_brg` int(11) NOT NULL,
+  `nama_brg` varchar(120) NOT NULL,
+  `keterangan` varchar(225) NOT NULL,
+  `kategori` varchar(60) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(4) NOT NULL,
+  `gambar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_barang`
+--
+
+INSERT INTO `tbl_barang` (`id_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`, `stok`, `gambar`) VALUES
+(1, 'Gerobak Bakso Minimalis', 'Unik Dan Berkualitas', 'Gerobak Bakso', 9000000, 3, 'bakso1.jpg'),
+(2, 'Gerobak Bakso Jendral', 'Ketika Memakan Bakso Ini Akan Merasakan Sensasinya', 'Gerobak Bakso', 7000000, 0, 'bakso2.jpg'),
+(3, 'Gerobak Bakso Kayu', 'Simple Dan Elegant', 'Gerobak Bakso', 2000000, 2, 'bakso3.jpg'),
+(4, 'Gerobak Bakso Kayu Manis ', 'Ramah Lingkungan Dan Simple Banget Gaes', 'Gerobak Bakso', 10000000, 1, 'bakso4.jpg'),
+(5, 'Gerobak Soto Putih Melati ', 'Gerobak Putih Melati Bermotif Garis Kuning', 'Gerobak Soto', 6000000, 3, 'soto1.jpg'),
+(6, 'Gerobak Soto Angkringan Cak Harmony', 'Simple Dan Santai Top Markotop', 'Gerobak Soto', 5500000, 2, 'soto2.jpg'),
+(7, 'Gerobak Soto Roda Biru', 'Roda Biru Dan Keren', 'Gerobak Soto', 20000000, 2, 'soto3.jpg'),
+(8, 'Gerobak Sate Modern', 'Model Seperti Perahu Layar', 'Gerobak Sate', 35000000, 3, 'sate1.jpg'),
+(9, 'Gerobak Sate Indie', 'Keren Simple Dan Elegant', 'Gerobak Sate', 8500000, 2, 'sate2.jpg'),
+(10, 'Gerobak Sate Taichan', 'Berwarna Hitam Pekat Melekat', 'Gerobak Sate', 3000000, 1, 'sate3.jpg'),
+(11, 'Gerobak Sate Jreng', 'Merah Merona Dan Mempesona', 'Gerobak Sate', 750000, 1, 'sate4.jpg'),
+(12, 'Gerobak Sate Cak Suna', 'Gerobak Cak Suna Sejak 1987', 'Gerobak Sate', 5000000, 1, 'sate5.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_invoice`
+--
+
+CREATE TABLE `tbl_invoice` (
+  `id_invoice` int(11) NOT NULL,
+  `nama` varchar(56) NOT NULL,
+  `alamat` varchar(225) NOT NULL,
+  `tgl_pesan` datetime NOT NULL,
+  `batas_bayar` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_invoice`
+--
+
+INSERT INTO `tbl_invoice` (`id_invoice`, `nama`, `alamat`, `tgl_pesan`, `batas_bayar`) VALUES
+(12, 'Bismillah', '325', '2020-11-06 22:57:26', '2020-11-07 22:57:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pesanan`
+--
+
+CREATE TABLE `tbl_pesanan` (
+  `id` int(11) NOT NULL,
+  `id_invoice` int(11) NOT NULL,
+  `id_brg` int(11) NOT NULL,
+  `nama_brg` varchar(50) NOT NULL,
+  `jumlah` int(3) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `pilihan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_pesanan`
+--
+
+INSERT INTO `tbl_pesanan` (`id`, `id_invoice`, `id_brg`, `nama_brg`, `jumlah`, `harga`, `pilihan`) VALUES
+(15, 12, 2, 'Gerobak Bakso Jendral', 1, 7000000, '');
+
+--
+-- Triggers `tbl_pesanan`
+--
+DELIMITER $$
+CREATE TRIGGER `pesanan_penjualan` AFTER INSERT ON `tbl_pesanan` FOR EACH ROW BEGIN
+	UPDATE tbl_barang SET stok = stok-NEW.jumlah
+    WHERE id_brg = NEW.id_brg;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role_id` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `nama`, `username`, `password`, `role_id`) VALUES
+(1, 'admin', 'admin', '123', 1),
+(2, 'user', 'user', '123', 2),
+(3, 'tole', 'tole', '123', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  ADD PRIMARY KEY (`id_brg`);
+
+--
+-- Indexes for table `tbl_invoice`
+--
+ALTER TABLE `tbl_invoice`
+  ADD PRIMARY KEY (`id_invoice`);
+
+--
+-- Indexes for table `tbl_pesanan`
+--
+ALTER TABLE `tbl_pesanan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_invoice` (`id_invoice`),
+  ADD KEY `id_brg` (`id_brg`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  MODIFY `id_brg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tbl_invoice`
+--
+ALTER TABLE `tbl_invoice`
+  MODIFY `id_invoice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tbl_pesanan`
+--
+ALTER TABLE `tbl_pesanan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_pesanan`
+--
+ALTER TABLE `tbl_pesanan`
+  ADD CONSTRAINT `tbl_pesanan_ibfk_1` FOREIGN KEY (`id_invoice`) REFERENCES `tbl_invoice` (`id_invoice`),
+  ADD CONSTRAINT `tbl_pesanan_ibfk_2` FOREIGN KEY (`id_brg`) REFERENCES `tbl_barang` (`id_brg`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
